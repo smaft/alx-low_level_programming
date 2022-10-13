1dffb6d8362c9291e84540161fe2a9d3aec1c1cd
smaft <litimsamil@gmail.com>
2022-10-13 04:08:45 -0700

    Task 4 -5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <studio.h>
 /**
- * main - entry
- * return always 0
- * 
+ * main - Entry
+ * Return :Always 0
  */
 int main(void)
 {
-	 puts ( "\"Programming is like building a multilingual puzzle");
-	 return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
