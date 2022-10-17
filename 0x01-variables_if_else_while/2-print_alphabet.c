53540bf0a765924ad80267952c652c26789cf2f8
smaft <litimsamil@gmail.com>
2022-10-17 06:10:42 -0700

    Task 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Main - print in lower case
+ * Return : always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++);
+	{
+	       putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
