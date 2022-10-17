d8b9950669340800d429da895ad0d8f122cf7865
smaft <litimsamil@gmail.com>
2022-10-17 07:36:29 -0700

    Task 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Main - print
+ * Return : Always 0
+ */
+int main(void)
+{
+	chal ll;
+
+	for (ll = 'z'; ll >= 'a'; ll--)
+		putchar(ll);
+
+	putchar('\n');
+
+	return (0);
+}
