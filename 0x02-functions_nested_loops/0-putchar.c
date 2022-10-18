fcbf5bc5cfd8960d4c98017ce69edecadd207d2c
smaft <litimsamil@gmail.com>
2022-10-18 06:48:01 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,16 @@
  * Main - prints _putchar, followed by a new line.
  * Return : Always 0 (success)
  */
-int main(viod)
+int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
-
-	return (0);
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
+return (0);
 }
