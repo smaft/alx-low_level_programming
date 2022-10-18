1b3ac626862067be11a289fab8fce725179dfe33
smaft <litimsamil@gmail.com>
2022-10-18 06:05:57 -0700

    Task 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Main - prints _putchar, followed by a new line.
+ * Return : Always 0 (success)
+ */
+int main(viod)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
+	return (0);
+}
