17e56601b4d4b6185475919312788abac34a4c56
smaft <litimsamil@gmail.com>
2022-10-18 07:04:48 -0700

    Task 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n');
+}
