636b4ae5afd96466473d61806e0e1d2b044a3318
smaft <litimsamil@gmail.com>
2022-10-19 07:14:02 -0700

    Task 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  */
 void print_alphabet_x10(void)
 {
@@ -14,5 +12,5 @@ void print_alphabet_x10(void)
 		for (la = 'a'; la <= 'z'; la++)
 			_putchar(la);
 		_putchar('\n');
-	}
+	i}
 }
