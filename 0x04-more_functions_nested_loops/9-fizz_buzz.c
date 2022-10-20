a44a88555b30675d85816ca73669738faf348477
smaft <litimsamil@gmail.com>
2022-10-20 07:29:25 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers from 1 to 100 But for multiples of
+ * three print Fiz
+ */
+int main(viod)
+{
+	int n;
+
+	for (n = 1; n <= 100, n++)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((n % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		if (n == 100)
+		{
+			continue;
+		}
+		printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
+
