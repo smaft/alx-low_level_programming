0dfe97e59535ef06b509dc4973ad4b6da6630754
smaft <litimsamil@gmail.com>
2022-10-27 03:39:27 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,18 +10,11 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0')
+	while (dest[i])
 	{
 		i++;
 	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-
-	dest[i] = '\0';
+	for (j = 0; src[j]; j++)
+		dest[i++] = src[j];
 	return (dest);
 }
