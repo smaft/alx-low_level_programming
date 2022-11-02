d1fc35279d23444b039428cf9ea32685d343b88b
smaft <litimsamil@gmail.com>
2022-11-02 07:32:51 -0700

    swap file delet

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.Return: The length of the string.
+ */
+
